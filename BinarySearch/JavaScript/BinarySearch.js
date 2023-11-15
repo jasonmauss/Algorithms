de61fc2e6a4d842fa7d6760be34e69fd211d66f3
Jason Mauss <jason.mauss@gmail.com>
2023-11-14 22:42:10 -0600

    additions to the code

--- BinarySearch/JavaScript/BinarySearch.js
+++ BinarySearch/JavaScript/BinarySearch.js
@@ -1,4 +1,13 @@
-const binarySearch = (inputValuesArray) => {
+const binarySearch = (inputValuesArray, valueToFind) => {
+
+    let left = 0;
+    let right = inputValuesArray.length - 1;
+
+    while(left <= right) {
+        let middle = Math.floor((left + right) / 2);
+    }
+    
+
 
 };
 
