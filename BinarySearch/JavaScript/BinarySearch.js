803132b75414b289c81001c538623977b461ae61
Jason Mauss <jason.mauss@gmail.com>
2023-11-15 23:48:37 -0600

    Added unit tests

--- BinarySearch/JavaScript/BinarySearch.js
+++ BinarySearch/JavaScript/BinarySearch.js
@@ -1,10 +1,12 @@
 const binarySearch = (inputValuesArray, valueToFind) => {
 
-    let left = 0;
-    let right = inputValuesArray.length - 1;
+    let leftPosition = 0;
+    let rightPosition = inputValuesArray.length - 1;
 
-    while(left <= right) {
-        let middle = Math.floor((left + right) / 2);
+    while(leftPosition <= rightPosition) {
+        let middlePosition = Math.floor((leftPosition + rightPosition) / 2);
+
+        
     }
     
 
@@ -13,3 +15,14 @@ const binarySearch = (inputValuesArray, valueToFind) => {
 
 // Unit Tests
 
+const searchArraySequential = [1, 2, 3, 4, 5, 6, 7];
+console.log(binarySearch(searchArraySequential, 4)); // expected: 3
+
+const searchArrayDuplicates = [1, 2, 3, 4, 4, 5, 6, 7];
+console.log(binarySearch(searchArrayDuplicates, 4)); // expected: 3 or 4
+
+const searchArraySkippedNumbers = [4, 10, 17, 22, 41, 50, 77, 102, 133, 196];
+console.log(binarySearch(searchArraySkippedNumbers, 50)); // expected: 5
+
+const searchArrayValueDoesntExist = [10, 25, 32, 49, 55, 61, 70];
+console.log(binarySearch(searchArrayValueDoesntExist, 42)); // expected: -1
